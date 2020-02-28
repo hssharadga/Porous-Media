a561f19ad3a6b5a829ef63ad95134cbfd49ea36b
hssharadga <60160133+hssharadga@users.noreply.github.com>
2020-02-28 16:27:54 -0600

    Compression&Packing

---
+++ Packing/advanced_packed.m
@@ -0,0 +1,35 @@
+
+clc
+clear all
+CC = readtable('Circels.csv');
+CC1 = readtable('Center_old.csv');
+
+for i=1:2
+
+
+CCC=CC{:,:}
+CCC1=CC1{:,:}
+
+if i==1   
+X=CCC(:,1)
+Y=CCC(:,2)
+r=50
+figure
+circles (X,Y,r)
+end
+
+if i==2
+XX=CCC1(:,1)
+YY=CCC1(:,2)
+r=50
+figure
+circles (XX,YY,r)  
+end
+
+axis([0 2483*4 0 2483/2])
+
+end
+
+ 
+
+
